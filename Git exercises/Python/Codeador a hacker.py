719d93ea0f79b101b389b526867c5191b776f7fe
Carlos Moraless <carlos52.7624@gmail.com>
2023-11-02 23:27:12 -0400

    Adding a commentary

--- Git exercises/Python/Codeador a hacker.py
+++ Git exercises/Python/Codeador a hacker.py
@@ -1,3 +1,12 @@
+
+""" Escribe un programa que reciba un texto y transforme lenguaje natural a
+"lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ se caracteriza por sustituir caracteres alfanuméricos.
+- Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+  con el alfabeto y los números en "leet".
+  (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+  """
+
 def translator(text):
 
     code = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#", "I": "1",
