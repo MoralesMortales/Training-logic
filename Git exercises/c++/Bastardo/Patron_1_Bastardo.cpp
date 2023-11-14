40ccef77b93a55b761e2caa5cbba4331445409b9
Carlos Moraless <carlos52.7624@gmail.com>
2023-11-14 01:05:58 -0400

    Mejoras en Bastardo's code

--- Git exercises/c++/Bastardo/Patron_1_Bastardo.cpp
+++ Git exercises/c++/Bastardo/Patron_1_Bastardo.cpp
@@ -7,17 +7,46 @@
 #include <iostream>
 
 int main(){
+ int a;
     char content[12][12];
     for (int f = 0; f < 12; f++){
             for (int s = 0; s < 12; s++){
                 content[f][s] = ' ';
+}
+}
+
+            for (int s = 0; s < 12; s++){
+            int f;
+
+            if (f == 0 || f == 1)
+            {
+               content[f][s] = 'X';
+               f++;
+            }
+
+            else if (f == 2)
+            {
+               content[f][s] = 'X';
+               f = f - 2;
+            }
+            
+            else{
+               f = 0;
+               content[f][s] = 'X';
+               f++;
+
+            }
+
+            }
+
+  for (int f = 0; f < 12; f++){
+            for (int s = 0; s < 12; s++){
                 std::cout<<"["<<content[f][s]<<"]";
                 if (s == 11){
                 std::cout<<"\n";
                 }
 }
 }
-
 
 return 0;
 }
